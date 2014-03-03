ae80ac16dd3f35010e0312dda4fc0883cde47743
b65155156 <b65155156@hotmail.com>
2014-03-03 18:52:41 +0800

    Revert "Revert "簽入README""
    This reverts commit 7ec30b54c767aed077d7af3ad3c225b2d15437d0.

---
+++ 2-2.java
@@ -0,0 +1,130 @@
+import java.io.*;    
+public class W2.2
+{
+	public static void main(String[] args)
+	{
+		string name;
+		int y=0,m=0,d=0; 
+		//op1
+		double a=0.0,b=0.0,sum=0.0;
+		while(ture)
+		{
+			system.out.print("1)練習一 2)練習二 3)練習三 4)練習四 5)離開");
+			Scanner n = new Scanner(System.in);
+			opt=n.nextInt();
+ 			if(opt==1)
+			{
+				BufferedReader a = new BufferedReader(new InputStreamReader(System.in));
+				System.out.print("請輸入姓名:"); 
+ 				name=a.readline();
+				Scanner y = new Scanner(System.in);
+				Scanner m = new Scanner(System.in);
+				Scanner d = new Scanner(System.in);
+				y-=1911;
+				System.out.println("您的名字:"+name+"\n生日 民國:"+y+"年"+m+"月"+d+"日");
+			}
+			if(opt==2)
+			{
+				while(ture)
+				{
+					system.out.print("1)+ 2)- 3)* 4)/ 5) 離開");
+					Scanner n2 = new Scanner(System.in);
+					opt2=n.nextInt();
+					switch(opt)
+					{
+						case 1:
+							sum=a+b;
+							System.out.println("the answer is"+sum);
+							break;	
+						case 2:
+							sum=a-b;
+							System.out.println("the answer is"+sum);
+							break;
+						case 3:
+							sum=a*b;
+							System.out.println("the answer is"+sum);
+							break;
+						case 4:
+							if(b==0)
+							{
+								System.out.println("error");
+								break;
+							}
+							else
+							{
+								sum=a/b;
+								System.out.println("the answer is"+sum);
+								break;
+							}
+						case 5:
+							break;
+					}
+				}
+			}	
+			if(opt==3)
+ 			{
+				system.out.print("請輸入西元年:");
+				Scanner n = new Scanner(System.in);
+				wyn=n.nextInt();
+				System.out.println("閏年:");
+				for(i:year)
+				{
+					if(i%400==0||(i%4==0 && i%100!=0))
+						System.out.print(i);
+				}
+			}	
+			if(opt==4)
+			{
+				stu[] stu=new stu[100];
+				int peonum=0,g=0;
+				while(ture)
+				{	
+					system.out.print("1)新增 2)刪除 3)離開");
+					Scanner n2 = new Scanner(System.in);
+					opt2=n2.nextInt();
+					if(opt==1)
+					{
+						while(ture)
+						{
+					 		System.out.println("請輸入學生姓名 以及成績:");
+							string name2=a.readline();
+							Scanner g = new Scanner(System.in);
+							stu[pronum]=new stu(name2,g)
+							pronum++;
+						}
+					}
+					if(opt==2)
+					{
+						while(1)
+						{
+							System.out.println("輸入編號;");
+							Scanner n = new Scanner(System.in);
+							num2=n.nextInt();
+							if(num2<=-1 || num2>pronum)
+							{
+								System.out.println("錯誤編號");
+							}
+							else
+							{
+								System.out.println("學生姓名"+"stu[num2].stuname+"學生成績"+stu[num2].stugra");
+							}
+						}
+					}
+				}
+			}
+			if(opt==5)
+			{break;}		
+		}		
+	
+	}
+}
+public class stu
+{
+	private string stuname;
+	private int stugra;
+	public stu(string name,int gra)
+	{
+		stuname=name;
+		stugra=gra;
+	}
+}
---
+++ 2-42.java
@@ -0,0 +1,121 @@
+abstract class Shape 
+{
+	abstract void print();
+}
+class Circle extends Shape 	
+{
+	Circle (double r)
+	{
+		this.r=a;
+	}
+	public void print()
+	{
+		System.out.println ("Circle  ,area:"+r*r*3.14);
+	}
+}
+class Oval extends Shape 	
+{
+	Oval (double a,double b)
+	{
+		this.a=a;
+		this.b=b;
+	}
+	public void print()
+	{
+		System.out.println ("Oval ,area:"+a*b*3.14);
+	}
+}
+class Rectangle extends Shape 	
+{
+	Rectangle (double a,double b)
+	{
+		this.a=a;
+		this.b=b;
+	}	
+	public void print()
+	{
+		System.out.println("Rectangle ,area:"+a*b);
+	}
+}
+class Triangle extends Shape 	
+{
+	double a, b;
+	Triangle(double a,double b)
+	{
+		this.a=a;
+		this.b=b;
+	}
+	public void print()
+	{
+		System.out.println("Triangle ,area:"+a*b/2);
+	}
+}
+
+public class Week2_42 
+{
+	public static void main(String[] args) 
+	{	
+		double a=0.0 , b=0.0;
+		ArrayList<Shape> arrayList=new ArrayList<Shape>();
+		Scanner scan=new Scanner(System.in);
+		int opt=0,c=0;
+		while(ture)
+		{
+			System.out.printIn("1)Triangle 2)Rectangle 3)Circle 4)Oval 5)exit:");
+			opt=scan.nextInt();
+			if(opt==1)
+			{
+				Triangle tri;
+				System.out.println("請輸入底:");
+				a=scan.nextDouble();
+				System.out.println("請輸入高:");
+				b=scan.nextDouble();
+				tri=new Triangle(a,b);
+				arrayList.add(tri);
+				tri=null;
+				c++;
+			}
+			if(opt==2)
+			{
+				Rectangle rec;
+				System.out.println("請輸入長:");
+				a=scan.nextDouble();
+				System.out.println("請輸入寬:");
+				b=scan.nextDouble();
+				rec=new Rectangle (a,b);
+				arrayList.add(rec);
+				rec=null;
+				c++;				
+			}
+			if(opt==3)
+			{
+				Circle cir;
+				System.out.println("請輸入半徑:");
+				a=scan.nextDouble();
+				cir=new Circle (a);
+				arrayList.add(cir);
+				cir=null;
+				c++;				
+			}
+			if(opt==4)
+			{
+				Oval ova;
+				System.out.println("請輸入長:");
+				a=scan.nextDouble();
+				System.out.println("請輸入寬:");
+				b=scan.nextDouble();
+				ova=new Oval(a,b);
+				arrayList.add(ova);
+				ova=null;
+				c++;	
+			}
+			if(opt==5)
+			{break;}
+			
+		}
+		for(int i=0;i<count;i++)
+		{
+			arrayList.get(i).show();
+		}
+	}
+}
